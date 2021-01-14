81a9f3d4583125dd1636d6d6bfbcc0482823659e
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-14 17:15:13 +0100

    Added setup getWorkout and -leaderboard

--- Leds_Run/Leds_Run/Leds_Run/repositories/RepoWorkout.cs
+++ Leds_Run/Leds_Run/Leds_Run/repositories/RepoWorkout.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Leds_Run.models;
 
 namespace Leds_Run.repositories
 {
     class RepoWorkout
     {
+
+
+        public async static Task<List<Workout>> GetDefaultWorkouts()
+        {
+            //default workouts opvragen
+            List<Workout> workouts = new List<Workout>();
+            return workouts;
+        }
+
+        public async static Task<List<Workout>> GetCostumWorkouts()
+        {
+            //Costumworkout opvragen
+            List<Workout> workouts = new List<Workout>();
+            return workouts;
+        }
+
+
+        //public async static Task<List<Leaderboard>> GetLeaderboard()
+        //{
+        //    //Leaderboard opvragen
+        //    List<Leaderboard> Leaderboards = new List<Leaderboard>();
+        //    return List<Leaderboard>
+        //}
+
     }
 }
