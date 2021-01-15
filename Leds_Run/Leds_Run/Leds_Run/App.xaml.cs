a721f8b6244e03a770972f2c2692e5d6c8439bed
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-15 15:50:00 +0100

    Added LeaderboardPage

--- Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
+++ Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Leds_Run
             InitializeComponent();
 
             //MainPage = new MainPage();
-            MainPage = new NavigationPage(new StartPage());
+            MainPage = new NavigationPage(new LeaderboardPage());
         }
 
         protected override void OnStart()
