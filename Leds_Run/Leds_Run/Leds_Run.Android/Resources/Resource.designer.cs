a14ae9baa3331b1c051625c7d88a088c145be4aa
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-19 21:47:00 +0100

    Finishing Leaderboard

--- Leds_Run/Leds_Run/Leds_Run.Android/Resources/Resource.designer.cs
+++ Leds_Run/Leds_Run/Leds_Run.Android/Resources/Resource.designer.cs
@@ -8363,6 +8363,9 @@ namespace Leds_Run.Droid
 			// aapt resource value: 0x7F070078
 			public const int tooltip_frame_light = 2131165304;
 			
+			// aapt resource value: 0x7F070079
+			public const int top_3_leaderboard = 2131165305;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
