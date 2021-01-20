797b1a0defa3e6232388812bafafe151ae5e3f13
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-20 17:30:12 +0100

    Added StartupPage and most of the xaml code

--- Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
+++ Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Leds_Run
             InitializeComponent();
 
             //MainPage = new MainPage();
-            MainPage = new NavigationPage(new StartPage());
+            MainPage = new NavigationPage(new StartupPage());
         }
 
         protected override void OnStart()
