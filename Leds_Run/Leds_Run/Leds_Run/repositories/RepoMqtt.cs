d35fdfde90f46a037a23c68832b82435576c597f
Arthur d'Hooge <arthur.dhooge@outlook.com>
2021-01-20 17:26:11 +0100

    Mqtt Handler finished

--- Leds_Run/Leds_Run/Leds_Run/repositories/RepoMqtt.cs
+++ Leds_Run/Leds_Run/Leds_Run/repositories/RepoMqtt.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Client;
 
 namespace Leds_Run.repositories
 {
     public class RepoMqtt
     {
+        private MqttFactory factory = new MqttFactory();
+        private IMqttClientOptions options;
+        private IMqttClient mqttClient;
 
+        public RepoMqtt()
+        {
+            options = new MqttClientOptionsBuilder().WithClientId(Guid.NewGuid().ToString()).WithTcpServer("52.174.57.98").WithCredentials("ledsrun", "N9A0zrF6XKdHRqRl").Build();
+            mqttClient = factory.CreateMqttClient();
+        }
+
+        public async void PublishMessage(string payload, string workoutId)
+        {
+            MqttApplicationMessage message = new MqttApplicationMessageBuilder().WithTopic($"workout/{workoutId}").WithPayload(payload).WithExactlyOnceQoS().Build();
+            await mqttClient.ConnectAsync(options);
+            await mqttClient.PublishAsync(message);
+            await mqttClient.DisconnectAsync();
+        }
     }
 }
