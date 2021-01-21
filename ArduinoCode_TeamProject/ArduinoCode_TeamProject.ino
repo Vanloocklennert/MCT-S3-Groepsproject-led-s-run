7cc2378cb20c22ff917d12c516d32da72b705720
RobbeBreens <robbe.breens@student.howest>
2021-01-21 01:29:57 +0100

    Added function to calculate delay between switching leds

--- ArduinoCode_TeamProject/ArduinoCode_TeamProject.ino
+++ ArduinoCode_TeamProject/ArduinoCode_TeamProject.ino
@@ -1,24 +1,46 @@
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(43, 6);
-
+const double distanceOneLed = 1.6; // cm  
 
 void setup() {
   strip.begin();
   strip.setBrightness(50);
   strip.clear();
   strip.show();
-
+  Serial.begin(9600);
 
   
 }
 
 void loop() {
 
-
+    Serial.println(ledDelayMs, 8);
     for(int x = 0;x<43;x++){
     strip.setPixelColor(x-1, 0,0,0);
     strip.setPixelColor(x , 255,0,0);
     strip.show();
-    delay((0.005 * 1000));
-  }
+    delay((ledDelayMs));
 }
+}
+
+
+
+
+float CalculateLedDelay(float velocity){
+    float velocityMeters = (velocity/3.6) * 100;
+    float ledDelay = (velocityMeters / distanceOneLed);
+    float ledDelaySecond = 1 / ledDelay;
+    float ledDelayMs = ledDelaySecond * 1000;
+
+    
+    return ledDelayMs; 
+}
+
+
+
+
+
+
+
+
+  
