ec46188d1171b3836f925036a05a728dd0270948
RobbeBreens <robbe.breens@student.howest>
2021-01-22 01:52:08 +0100

    Python mqtt finished, serial communication between rpi and arduino finished.

--- ArduinoCode_TeamProject/ArduinoCode_TeamProject.ino
+++ ArduinoCode_TeamProject/ArduinoCode_TeamProject.ino
@@ -1,6 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(43, 6);
 const double distanceOneLed = 1.6; // cm  
+int workoutData [10][2];
+
+ float  beginTime1;
+ float beginTime2;
 
 void setup() {
   strip.begin();
@@ -9,18 +13,52 @@ void setup() {
   strip.show();
   Serial.begin(9600);
 
-  
+  beginTime1 = millis();
+  beginTime2 = millis(); 
+ 
 }
 
 void loop() {
+ if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('#');
 
-    Serial.println(ledDelayMs, 8);
-    for(int x = 0;x<43;x++){
-    strip.setPixelColor(x-1, 0,0,0);
-    strip.setPixelColor(x , 255,0,0);
-    strip.show();
-    delay((ledDelayMs));
-}
+    Serial.println(data);
+    String workoutVelocity = seperateString(data, '&', 0);
+    String workoutTime = seperateString(data, '&', 1);
+
+    Serial.println(workoutVelocity);
+
+    for (int x = 0; x < 10; x++){
+        if(workoutData[x] == ""){
+            Serial.println(workoutVelocity);
+            Serial.println(workoutTime);
+            workoutData[x][0] =   workoutVelocity.toFloat();
+            workoutData[x][1] = workoutTime.toFloat();
+            break;
+        }
+    } 
+ }
+
+
+
+
+float elapsedTime1 = millis() - beginTime1;
+float elapsedTime2 = millis() - beginTime2;
+
+int elapsedLeds = round(elapsedTime1 / 26);
+int elapsedLeds2 = round(elapsedTime2 / 25);
+
+
+if (elapsedLeds > 42) beginTime1 = millis();
+if (elapsedLeds2 > 42) beginTime2 = millis();
+
+strip.setPixelColor(elapsedLeds - 1, 0,0,0);
+strip.setPixelColor(elapsedLeds, 255,0,0);
+
+strip.setPixelColor(elapsedLeds2 - 1, 0,0,0);
+strip.setPixelColor(elapsedLeds2, 0,255,0);
+strip.show();
+
 }
 
 
@@ -34,6 +72,23 @@ float CalculateLedDelay(float velocity){
 
     
     return ledDelayMs; 
+}
+
+
+String seperateString(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
 
---
+++ Raspberry/teamproject.py
@@ -0,0 +1,53 @@
+import serial
+import time
+import paho.mqtt.client as mqtt
+import json
+
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected To server "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("workout/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("Topic:" + msg.topic+" Payload:"+str(msg.payload))
+    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+
+    msgString = "{0}&{1}#".format(str(json.loads(msg.payload)["speed"]), str(json.loads(msg.payload)["time"]))
+    ser.flush()
+    ser.write(str.encode(msgString))
+
+
+client = mqtt.Client()
+client.username_pw_set("ledsrun", password="N9A0zrF6XKdHRqRl")
+
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("52.174.57.98", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
