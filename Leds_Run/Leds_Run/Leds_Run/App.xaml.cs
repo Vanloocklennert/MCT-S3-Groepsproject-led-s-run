4f8bd407a4db186f75bc925cb2da01e71d38f1d1
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-24 00:48:42 +0100

    Made Layout and timer

--- Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
+++ Leds_Run/Leds_Run/Leds_Run/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Leds_Run
             InitializeComponent();
 
             //MainPage = new MainPage();
-            MainPage = new NavigationPage(new StartPage());
+            MainPage = new NavigationPage(new StartWorkoutPage());
         }
 
         protected override void OnStart()
