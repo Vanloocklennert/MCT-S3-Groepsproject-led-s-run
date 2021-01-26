d740fab762d0b20b26d0b70197e09f6ee5d9780c
lennert van loock <lennert.van.loock@student-howest.be>
2021-01-26 19:29:32 +0100

    Finished NewWorkout

--- Leds_Run/Leds_Run/Leds_Run/views/Interval.xaml.cs
+++ Leds_Run/Leds_Run/Leds_Run/views/Interval.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Leds_Run.models;
 
 namespace Leds_Run.views
 {
@@ -20,13 +21,52 @@ namespace Leds_Run.views
         private void pcType_SelectedIndexChanged(object sender, EventArgs e)
         {
             EntryDurDis.IsEnabled = true;
-            if (pcType.SelectedItem == "runspeedtime")
+            if (pcType.SelectedItem == "Distance")
             {
                 lblDurDis.Text = "meter";
             }
             else
             {
                 lblDurDis.Text = "minuten";
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double speed = Convert.ToDouble(EntryPace.Text);
+            string type;
+            double Distance, Speed;
+            TimeSpan time;
+            if (EntryDurDis.IsEnabled)
+            {
+                if(speed != 0)
+                {
+                    if (pcType.SelectedItem == "Distance")
+                    {
+                        type = "rundistancespeed";
+                        Speed = Convert.ToDouble(EntryPace.Text)/3.6;
+                        Distance = Convert.ToDouble(EntryDurDis.Text);
+                        time = time.Add(TimeSpan.FromSeconds(Distance / Speed));
+                    }
+                    else
+                    {
+                        type = "runspeedtime";
+                        Speed = Convert.ToDouble(EntryPace.Text)/3.6;
+                        time = time.Add(TimeSpan.FromMinutes(Convert.ToDouble(EntryDurDis.Text)));
+                        Distance = Speed*time.TotalSeconds;
+
+                    }
+                    Workout.Interval interval = new Workout.Interval();
+                    interval.Type = type;
+                    interval.Speed = Speed;
+                    interval.Distance = Distance;
+                    interval.Time = time;
+                    interval.Name = "";
+
+                    MessagingCenter.Send(this, "interval", interval);
+                    Application.Current.MainPage.Navigation.PopAsync();
+                }
+                
             }
         }
     }
